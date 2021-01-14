e3453840cb62875ed6766f45295eb5ee653da2d8
Danilo Nakai <contact.danilonakai@gmail.com>
2021-01-14 23:22:32 +0900

    Layout improvements

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 var id = 0;
-var output_content = "";
 var locations_list = [];
 
 function call_api(location){
@@ -43,42 +42,42 @@ function delete_location(id){
 function generate_card(id,title,temperature,img,country,description){
     let result = '<div class="card"><div class="title"><h2>'+
             title+' - '+country+
-            '</h2><i class="far fa-window-close" onclick="delete_location('+id+')"></i></div><div class="info"><span class="temperature">'+
+            '</h2><i class="fas fa-times" onclick="delete_location('+id+')"></i></div><div class="info"><span class="temperature">'+
             temperature+
-            '°C</span><figure><img src="'+img+'" alt=""><figcaption>'+description+
+            '°C</span><figure><img src="http://openweathermap.org/img/wn/'+img+'@2x.png" alt=""><figcaption>'+description+
             '</figcaption></figure></div></div>';
     
     return result;
 }
 
 function update_content(){
-    output_content = "";
-
-    $(locations_list).each(function(e){
-        let location = locations_list[e];
-        
-        call_api(location.title).then(function(response){
+    let output_content = "";
+    asyncForEach(locations_list, async function(location){
+        await call_api(location.title).then(function(response){
             return response.json();
         }).then(function(response){
             let id = location.id;
             let title = response.name; // title
-            let temperature = response.main.temp; //temperature
-            let img = ""; //img
+            let temperature = Math.round(response.main.temp); //temperature
+            let img = response.weather[0].icon; //img
             let description = response.weather[0].main; //description
             let country = response.sys.country;
 
-            let card = generate_card(id,title,temperature,img,country,description);
-            output_content = output_content + card;
+            return generate_card(id,title,temperature,img,country,description);
+        }).then(function(response){
+            output_content = output_content + response;
             $(".content").html(output_content);
             return;
         });
     });
 }
 
-
-function update_view(card){
-    
+async function asyncForEach(array, callback) {
+    for (let index = 0; index < array.length; index++) {
+        await callback(array[index], index, array);
+    }
 }
+  
 
 $('#add_location').click(function(e){
     e.preventDefault();
